be70bb6de70e79f5a7d3ef803e8a26fdf2aef499
barteko <haldman123@gmail.com>
2024-03-19 20:52:36 +0100

    Fix bugs

--- KNN-algorithm/main.py
+++ KNN-algorithm/main.py
@@ -34,7 +34,7 @@ def make_vectors(data):
 
 
 def get_k_smallest(vectors, k):
-    return sorted(vectors.keys())[:k]
+    return sorted(vectors.keys())[:k+1]
 
 
 def get_verdiction(vectors, dict, k):
@@ -52,12 +52,21 @@ def print_classified_vector(distance, smallest, k):
         dist[i] = (distance[smallest[i]])
     return dist
 
+
+def print_distance_dict(distance_dict):
+    count_vectors = 1
+    for k, v in distance_dict.items():
+        count_vectors = count_vectors + 1
+        print(k , v)
+    print(count_vectors)
+
 def knn_classification(training_data, testing_data, k):
     training_data_vectors = make_vectors(training_data)
     testing_data_vectors = make_vectors(testing_data)
 
-    distance_dict = {}
-    classifications = {}
+    distance_dict = {} ## slownik z parami [odlegosc --> tuple(zaklasyfikowany kwiat, podany testowy kwiat)]
+    classifications = {} ## klasyfikacje dla danych testowych
+    losted_nieghbourhoods = [] ## zgubieni sasiedzi przez nadpisanie sasiada gdy dzieli ich ta sama odleglosc
 
     i = 1
     for vec1 in testing_data_vectors:
@@ -65,15 +74,23 @@ def knn_classification(training_data, testing_data, k):
             for vec2 in training_data_vectors:
                 for k2, v2 in vec2.items():
                     distance = euclidean_distance(k1, k2)
+                    if distance in distance_dict:
+                        losted_nieghbourhoods.append({distance: tuple([v1, v2])})
                     distance_dict[distance] = tuple([v1, v2])
+            print( losted_nieghbourhoods, "ZGUBIENI SOMSIEDZI")
             k_smallest = get_k_smallest(distance_dict, k)
-           ## print("Dla wiersza ", i, " -> ", k_smallest, print_classified_vector(distance_dict, k_smallest, k))
-            print("Classification based on training data -> ", get_verdiction(k_smallest, distance_dict, k)[1])
-            print("input data -> ", v1)
-            print("")
+            print(len(distance_dict), "<----------- WIELKOSC DISTANCE_DICT")
+            print(len(k_smallest), "<----------- WIELKOSC k_SMALLEST")
+
+            ##            print("Dla wiersza ", i, " -> ", k_smallest, print_classified_vector(distance_dict, k_smallest, k))
+            ##print("Classification based on training data -> ", get_verdiction(k_smallest, distance_dict, k)[0])
+            ##print("input data -> ", v1)
+            ##print("")
             classifications[i] = (v1 == get_verdiction(k_smallest, distance_dict, k)[1])
             i = i + 1
+            ##print_distance_dict(distance_dict)
             distance_dict = {}
+        losted_nieghbourhoods = []
 
     positive_classification = 0
     for k, v in classifications.items():
@@ -86,15 +103,18 @@ def knn_classification(training_data, testing_data, k):
 
 training_data = read_data("iris_training.txt")
 testing_data = read_data("iris_test.txt")
-
-k = input("Input value of K: ")
-
-print("\nAccuracy level:", knn_classification(training_data, testing_data, int(k))*100)
 while True:
-    vector = input("\n\nInput vector in the form:\n-------------------------\n 5.1  3.5  1.4  0.2 ...\n-------------------------\nInput:")
-    vector = vector + " vector"
-    k = input("Input value of K:")
-    print("\n------------------------------------------------------------------------")
-    print("Accuracy level:", knn_classification(training_data, vector, int(k))*100, "\n------------------------------------------------------------------------")
+    k = input("Input value of K: [Or type input]")
+    if k != "input":
+        print("\nAccuracy level:", knn_classification(training_data, testing_data, int(k))*100)
+    else:
+        while True:
+            vector = input("\n\nInput vector in the form:\n-------------------------\n 5.1  3.5  1.4  0.2 ...\n-------------------------\nInput:")
+            vector = vector + " vector"
+            k = input("Input value of K: [Or type exit]")
+            if k == "exit":
+                break
+            print("\n------------------------------------------------------------------------")
+            print("Accuracy level:", knn_classification(training_data, vector, int(k))*100, "\n------------------------------------------------------------------------")
 
 
