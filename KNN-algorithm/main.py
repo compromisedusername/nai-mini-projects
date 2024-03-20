9c0657c063d0e1d6ab8b4492a122d77a373289aa
barteko <haldman123@gmail.com>
2024-03-20 15:12:27 +0100

    Done

--- KNN-algorithm/main.py
+++ KNN-algorithm/main.py
@@ -53,8 +53,8 @@ def get_verdiction(sorted_list_of_distance, k):
         if k > max_count:
             max_count = k
             classified_data = v
-        print(v,k, "<-----ELEMENTY")
-    print(classified_data,"<-----CLASSIFIED, ELEMENTS: ", max_count)
+        #print(v,k, "<-----ELEMENTY")
+    #print(classified_data,"<-----CLASSIFIED, ELEMENTS: ", max_count)
     return classified_data
 
 
@@ -79,7 +79,10 @@ def knn_classification(training_data, testing_data, k):
             classified_output = get_verdiction(list_of_distance, k)
 
             classifications[i] = (v1 == classified_output)
-            print("Testing data: " ,v1,", Classified data: ", classified_output)
+            if v1 != "vector":
+                print("Testing data: " ,v1,", Classified data: ", classified_output)
+            else:
+                print("Classified data: ", classified_output)
             i = i + 1
             list_of_distance = []
     positive_classification = 0
@@ -90,22 +93,55 @@ def knn_classification(training_data, testing_data, k):
 
 print("INPUT --> input your own vector")
 print("EXIT --> use file")
-training_data = read_data("iris_training.txt")
-testing_data = read_data("iris_test.txt")
+import os
+
+print("\nFILES:\n--------------------")
+cwd = os.getcwd()
+for filename in os.listdir(cwd):
+  if(filename.endswith(".txt")):
+    print(filename)
+print("--------------------\n")
+file_training=input("Input training file name: ")
+file_testing=input("Input testing file name: ")
+if(file_training == ""):
+    file_training = "iris_training.txt"
+if(file_testing == ""):
+    file_testing = "iris_test.txt"
+
+if( not os.path.isfile(file_testing)):
+    print(f"File \'{file_testing}\' doesnt exists! Used file: \'iris_test.txt\'")
+    file_testing="iris_test.txt"
+if( not os.path.isfile(file_training)):
+    print(f"File \'{file_training}\' doesnt exists! Used file: \'iris_training.txt\'")
+    file_training="iris_training.txt"
+training_data = read_data(file_training)
+testing_data = read_data(file_testing)
 while True:
-    k = input("Input value of K [or type input]: ")
+
+
+    k = input("-------DATA FROM FILES-------\nInput value of K [or type input]: ")
     if k != "input":
         print("\nAccuracy level:", knn_classification(training_data, testing_data, int(k))*100)
     else:
         while True:
-            vector = input("\n\nInput vector in the form:\n-------------------------\n 5.1  3.5  1.4  0.2 ...\n-------------------------\nInput values [or type exit]: ")
+            with open(file_training, "r") as f:
+                line = f.readline()
+                dimension = len(line.split()) - 1
+            print("\nVector must be in ", dimension, "dimension!")
+            vector = input("Input vector in the form:\n-------------------------\n 5.1  3.5  1.4  0.2 "
+                           "...\n-------------------------\nInput values [or type exit]: ")
             if vector == "exit":
                 break
-            vector = vector + " vector"
-            k = input("Input value of K [Or type exit]: ")
+            while (len(vector.split())) != dimension:
+                print("Vector must be in ", dimension, "dimension!")
+                vector = input("Input vector: ")
 
-            print("\n------------------------------------------------------------------------")
+            vector = vector + " vector"
+            k = input("Input value of K [or type exit]: ")
+            if k == "exit":
+                break
+            print("\nCLASSIFICATION:---------------------------------------------------------")
             knn_classification(training_data, vector, int(k))
-            print("\n------------------------------------------------------------------------")
+            print("\nEND---------------------------------------------------------------------")
 
 
