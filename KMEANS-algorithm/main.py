7f8409674d90644fee7775464df7c9755547fd9a
compromisedusername <haldman123@gmail.com>
2024-05-08 00:22:54 +0200

    Fix kmeans algorithm

--- KMEANS-algorithm/main.py
+++ KMEANS-algorithm/main.py
@@ -22,8 +22,50 @@ def make_vectors(data):
         vector.append(make_vector(line))
     return vector
 
-
 def kmeans_algorithm(vectors):
+
+    k_groups = int(input("Input number of groups: "))
+    if k_groups > len(vectors):
+        print("Too many groups for given data set!")
+        return
+
+    centroids = make_initial_centroids(k_groups, vectors)
+    print(centroids, "CENTROIDS")
+
+    square_distances = 0
+    clusters = {}
+    changes = 0
+
+    while True:
+        for vec, att in vectors:
+            distances = []
+            for cluster_number in range(0, len(centroids)):
+                distance = euclidean_square_distance(vec, centroids[cluster_number])
+                distances.append([vec, distance, cluster_number])
+
+            min_distance = min(distances, key=lambda x: x[1])
+            square_distances += min_distance[1]
+
+            #print(square_distances, "SQUARE DISTANCE")
+
+            cluster_id = min_distance[2]
+
+            if cluster_id not in clusters:
+                clusters[cluster_id] = [min_distance]
+            else:
+                clusters[cluster_id].append(min_distance)
+
+        check_centroids = centroids.copy()
+
+        for cluster_id, points in clusters.items():
+            new_centroid = calculate_new_centroid(points)
+            centroids[cluster_id] = new_centroid
+        print(check_centroids)
+        print(centroids)
+        if check_centroids == centroids:
+            break
+    return clusters
+def kmeans_algorithm2(vectors):
 
     k_groups = int(input("Input number of groups: "))
     if(k_groups > len(vectors)):
@@ -33,37 +75,36 @@ def kmeans_algorithm(vectors):
     print(centroids, "CENTROIDS")
 
     square_distances = 0
-    vector_set = set(([tuple(x[0]) for x in vectors]))
     clusters = {}
+    changes = 0
+
     while True:
-        for vec in vector_set: # dla kazdego wektora
-            changes = 0
-            distances = [] # dystanse dla kazdego wektora od danego centroidu
-            for cluster_number in range(0, len(centroids)):
-                distance = euclidean_square_distance(vec, centroids[cluster_number])
-                distances.append([vec, distance, cluster_number])
-                if cluster_number in clusters:
-                    new_centroid = calculate_new_centroid(clusters[cluster_number])
-                    if centroids[cluster_number] == new_centroid: # zmiany odleglosci
-                        changes += 1
+        for vec,att in vectors: # dla kazdego wektora ze zbioru
+            distances = [] # lista dystansow punkt -> centroid
+            for cluster_number in range(0, len(centroids)): # dla kazdego centroidu
+                distance = euclidean_square_distance(vec, centroids[cluster_number]) # dystans wektor -> centroid
+                distances.append([vec, distance, cluster_number]) # lista dystansow wektor -> wszystkie centroidy [wektor, dystans, numer klastra]
+                if cluster_number in clusters: # jesli dany klaster jest w slowniku klastrow
+                    new_centroid = calculate_new_centroid(clusters[cluster_number]) # liczmy nowy centroid dla konkretnego klastra
+                    if centroids[cluster_number] == new_centroid: # jesli ....
+                        1#changes += 1
                     centroids[cluster_number] = new_centroid # aktualizujemy centroid
-                    print(new_centroid, "NOWY CENTROID")
 
-
-
-            min_distance = min(distances, key=lambda x: x[1])
-            square_distances += min_distance[1]
+            min_distance = min(distances, key=lambda x: x[1]) # najmniejszy dystans od centroidu, x[1] to odleglosc z listy distances
+            square_distances += min_distance[1] # dodajemy najmniejsza odleglosc do sumy kwadratow odleglosci
 
             print(square_distances, "SQUARE DISTANCE")
 
-            cluster_id = min_distance[2]
+            cluster_id = min_distance[2] # min_distance[2] to numer klastra. clusters to slownik z kluczami [0....k_groups]
 
-            if cluster_id not in clusters:
+            if cluster_id not in clusters: # jesli slownik clusters nie posiada jeszcze zadnych punktrow, inicjujemy go
                 clusters[cluster_id] = [min_distance]
-            else:
-                clusters[cluster_id].append(min_distance)
+            else: # slownik clusters posiada juz minimum jeden punkt
+                clusters[cluster_id].append(min_distance) # dodajemy najmniejsze punkty do klastra
 
-            if changes >= k_groups-1:
+            changes += 1
+            print(centroids, "CENTROIDY")
+            if changes >= 150:
                 return clusters
 
 
@@ -128,7 +169,9 @@ if (not os.path.isfile(file)):
     file = "../iris_training.txt"
 data = read_data(file)
 vectors = make_vectors(data)
-
-kmeans_algorithm(vectors)
+result = kmeans_algorithm(vectors)
+print("-------------------------------RESULTS------------------------------")
+for elem in result.items():
+    print(len(elem[1]))
 
 
