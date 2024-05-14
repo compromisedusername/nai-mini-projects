f341751c75d07e40ce3ec59c9866f0d3fd516951
Bartlomiej Ossowski <haldman123@gmail.com>
2024-05-14 21:46:37 +0200

    Almost finish

--- NAIVE-BAYES-algorithm/main.py
+++ NAIVE-BAYES-algorithm/main.py
@@ -1,6 +1,4 @@
 import math
-import os
-import random
 
 
 def read_data(file):
@@ -11,11 +9,18 @@ def read_data(file):
     return tekst
 
 
+def euclidean_distance(p, q):
+    squared_sum = 0
+    for i in range(len(p)):
+        diff = float(p[i]) - float(q[i])
+        squared_sum += diff * diff
+    return math.sqrt(squared_sum)
+
+
 def make_vector(data):
     dict = {}
     column = data.split()
     vector = column[:-1]
-    vector.append(-1)
     dict[tuple(vector)] = (column[-1])
     return dict
 
@@ -27,140 +32,145 @@ def make_vectors(data):
     return vector
 
 
-def dot_product(w, t):
-    dot_factor = 0.0
-    for i in range(0, len(w)):
-        dot_factor = dot_factor + float(w[i]) * float(t[i])
-    ##print(dot_factor, "<------DOT_FACTOR")
-    return dot_factor
+def get_verdiction(sorted_list_of_distance, k):
+    k_smallest_elements = sorted_list_of_distance[:k]
+    count_attribut = {}
+    print(k_smallest_elements)
+    for i in k_smallest_elements:
+        data = i['data']['training']
+        if data not in count_attribut:
+            count_attribut[data] = 1
+        else:
+            count_attribut[data] += 1
 
 
-def sum_vectors(w, t):
-    new_w = []
-    for i in range(0, len(w)):
-        new_w.append(float(w[i]) + float(t[i]))
-    ##print(new_w, "<------NEW WECTOR")
-    return new_w
+    classified_data = ""
+    max_count = 0
+
+    sorted_count_attribut = sorted(count_attribut.items(), key=lambda x: (x[1], x[0]))
+
+    for v,k in sorted_count_attribut:
+        if k > max_count:
+            max_count = k
+            classified_data = v
+        #print(v,k, "<-----ELEMENTY")
+    #print(classified_data,"<-----CLASSIFIED, ELEMENTS: ", max_count)
+    return classified_data
 
 
-def scale_matrix(matrix, x):
-    scaled_matrix = []
-    for i in range(0, len(matrix)):
-        scaled_matrix.append(float(matrix[i]) * x)
-    return scaled_matrix
+def count_attributes(training_data):
+    attribute_count = {}
+    for vec in training_data:
+        for vector, attribute in vec.items():
+            if attribute not in attribute_count:
+                attribute_count[attribute] = 1
+            else:
+                attribute_count[attribute] += 1
+    return attribute_count
 
-
-def new_weight(w, t, alfa, decision_factor):
-    return sum_vectors(w, scale_matrix(t, alfa * decision_factor))
-
-
-def calculate_weights(training_data_vectors, alfa, classname, weight):
-    weights_not_change = 0
-    old_weight = []
-    while weights_not_change < 1:
-        for vector in training_data_vectors:
-            for v, k in vector.items():
-                old_weight = weight
-                net = dot_product(weight, v)
-                if net >= 0:
-                    output = 1
+def probabilites(training_data):
+    attribute_count = count_attributes(training_data)
+    dict_probabilites = {} # " i[...]attribute "
+    for vec in training_data:
+        for vector, attribute in vec.items():
+            for i in range(0,len(vector)):
+                key = str(i)+str(vector[i])+str(attribute)
+                if key not in dict_probabilites:
+                    dict_probabilites[key] = 1
                 else:
-                    output = 0
-                if ((output == 1) & (k != classname)):
-                    weight = new_weight(weight, v, alfa, -1)
-                    print(v, k, "Output 1, but attribute is other than Iris-setosa!")
-                elif ((output == 0) & (k == classname)):
-                    weight = new_weight(weight, v, alfa, 1)
-                    print(v, k, "Output 0, but attribute is  Iris-setosa!")
-        if (old_weight == weight):
-            weights_not_change = weights_not_change + 1
-            print("NEW WEIGHT: ", weight)
-    print("----CALCULATING WEIGHT FINISHED----[", weights_not_change, "]")
-    return weight
+                    dict_probabilites[key] += 1
+    unique_attribute_amount = len(attribute_count)
+
+    # tutaj zaczniemy wygladzac dane
+    for key_p, val_p in dict_probabilites.items(): # klucz i wartosci w slowniku ktory przechowuje  pod kluczem [ 'i' 'val' 'attribute' ] ich ilosci wystapien *i to nr kolumny*
+        dict_probabilites[key_p] += 1 # dodajemy jedynke przy wygladzaniu
+        for key_c, val_p in attribute_count.items(): # klucz i wartosc w slowniku przechowujacy pod kluczem attribute ich ilosci wystapien
+            if key_c in key_p:
+                dict_probabilites[key_p] /= (attribute_count[key_c] + unique_attribute_amount) # do mianownika dodajemy ilosc unikatowych atrybutow
+
+    return dict_probabilites, attribute_count
 
 
-def delta_algorithm(training_data, testing_data, alfa):
+def naive_bayes_classification(training_data, testing_data):
+
+
     training_data_vectors = make_vectors(training_data)
     testing_data_vectors = make_vectors(testing_data)
-    ## utworzenie wektorow
 
-    weight = []
-    for i in training_data_vectors[0].keys():
-        for j in i:
-            weight.append(random.uniform(-1, 1))
-    initial_weight = weight
+    attribute_count = probabilites(make_vectors(training_data))[1]
+    smoothed_probabilites = probabilites(make_vectors(training_data))[0]
 
-    weight = calculate_weights(training_data_vectors, alfa, "Iris-setosa", weight)
-    classifications = 1
-    data_rows = 1
-    for vector in testing_data_vectors:
-        for v, k in vector.items():
-            net = dot_product(weight, v)
-            if (net >= 0) & (k == "Iris-setosa"):
-                print("Positive classification ", v, k, ", Output -> 1")
-                classifications = classifications + 1
-            elif (net < 0) & (k != "Iris-setosa"):
-                print("Positive classifaction ", v, k, ", Output -> 0")
-                classifications = classifications + 1
-            else:
-                print("Not positive classification ", v, k, "Output ->", net)
-        data_rows = data_rows + 1
-    accuracy = classifications / data_rows
-    print("Accuracy: ", accuracy * 100, "&")
-    return weight
+    classified_output = 0
+    classifications = {} ## klasyfikacje dla danych testowych
+    i = 1
+    for vec1 in testing_data_vectors: # dla kazdego wektora testowego ...
+        probability_every_attribute = {}
+        for attribute_c, amount_count in attribute_count.items(): # dla kazdego atrybutu ze zbioru atrybutow i ich wystapien...
+            for vector_test, attribute_v in vec1.items(): # dla kazdej wartosci i atrybutu ze zbioru testowego ...
+                for i in range(0, len(vector_test)): # klucz i wartosci w slowniku ktory przechowuje  pod kluczem [ 'i' 'val' 'attribute' ] ich ilosci wystapien *i to nr kolumny*
+                        key = str(i)+str(vector_test[i]) + str(attribute_c)
+                        if key in smoothed_probabilites:
+                            if attribute_c not in probability_every_attribute:
+                                probability_every_attribute[attribute_c] = (smoothed_probabilites[key])
+                            else:
+                                probability_every_attribute[attribute_c] *= (smoothed_probabilites[key])
+                        else:
+                            if attribute_c not in probability_every_attribute:
+                                probability_every_attribute[attribute_c] = (1/attribute_count[attribute_c])
+                            else:
+                                probability_every_attribute[attribute_c] *= (1/attribute_count[attribute_c])
+        print(probability_every_attribute)
 
 
-print("INPUT --> input your own vector")
-print("EXIT --> use file")
+def main():
+    print("INPUT --> input your own vector")
+    print("EXIT --> use file")
+    import os
 
-cwd = os.getcwd()
-print('\nFILES in ', cwd, ' :\n--------------------')
+    print("\nFILES:\n--------------------")
+    cwd = os.getcwd()
+    for filename in os.listdir(cwd):
+        if (filename.endswith(".txt")):
+            print(filename)
+    print("--------------------\n")
+    file_training = input("Input training file name: ")
+    file_testing = input("Input testing file name: ")
+    if (file_training == ""):
+        file_training = "iris_training.txt"
+    if (file_testing == ""):
+        file_testing = "iris_test.txt"
 
-for filename in os.listdir(cwd):
-    if (filename.endswith(".txt")):
-        print(filename)
-print("--------------------\n")
-file_training = input("Input training file name [Or type enter to use standard set]:")
-file_testing = input("Input testing file name [Or type enter to use standard set]:")
-if (file_training == ""):
-    file_training = "../iris_training.txt"
-if (file_testing == ""):
-    file_testing = "../iris_test.txt"
-
-if (not os.path.isfile(file_testing)):
-    print(f"File \'{file_testing}\' doesnt exists! Used file: \'iris_test.txt\'")
-file_testing = "../iris_test.txt"
-if (not os.path.isfile(file_training)):
-    print(f"File \'{file_training}\' doesnt exists! Used file: \'iris_training.txt\'")
-file_training = "../iris_training.txt"
-training_data = read_data(file_training)
-testing_data = read_data(file_testing)
-with open(file_training, "r") as f:
-    line = f.readline()
-    dimension = len(line.split()) - 1
-alfa = (input("Do you want to input learning rate (alfa)? Type [y/n]"))
-while True:
-    if (alfa == "y"):
-        alfa = float(input("Input learning rate: "))
-    else:
-        alfa = 0.056789
-    weight = delta_algorithm(training_data, testing_data, 0.01)
-    next_print = input("Type e for exit: ")
-    if (next_print == "e"):
-        break;
+    if (not os.path.isfile(file_testing)):
+        print(f"File \'{file_testing}\' doesnt exists! Used file: \'iris_test.txt\'")
+        file_testing = "iris_test.txt"
+    if (not os.path.isfile(file_training)):
+        print(f"File \'{file_training}\' doesnt exists! Used file: \'iris_training.txt\'")
+        file_training = "iris_training.txt"
+    training_data = read_data(file_training)
+    testing_data = read_data(file_testing)
     while True:
-        print(f"Input vector in the form:\n-------------------------\n [EXAMPLE] --> : 5.1  3.5 ... "
-              "\n-------------------------\nInput values [in ", dimension, "- DIMENSIONS] [or type exit] ")
-        vector = input(":")
-        if vector == "exit":
-            break
-        while (len(vector.split())) != dimension:
-            print("Vector must be in ", dimension, "dimension!")
-            vector = input("Input vector: ")
-        vector = vector + " -1"
-        float_vector = vector.split(" ")
-        if (dot_product(weight, float_vector) >= 0):
-            print(vector," CLASSIFIED AS Iris-setosa")
-        else:
-            print(vector, " CLASSIFIED AS NON Iris-setosa")
+
+        print("\nAccuracy level:", naive_bayes_classification(training_data, testing_data) * 100)
+        while True:
+            with open(file_training, "r") as f:
+                line = f.readline()
+                dimension = len(line.split()) - 1
+            print("\nVector must be in ", dimension, "dimension!")
+            vector = input("Input vector in the form:\n-------------------------\n 5.1  3.5  1.4  0.2 "
+                           "...\n-------------------------\nInput values [or type exit]: ")
+            if vector == "exit":
+                break
+            while (len(vector.split())) != dimension:
+                print("Vector must be in ", dimension, "dimension!")
+                vector = input("Input vector: ")
+
+            vector = vector + " vector"
+            k = input("Input value of K [or type exit]: ")
+            if k == "exit":
+                break
+            print("\nCLASSIFICATION:---------------------------------------------------------")
+            naive_bayes_classification(training_data, vector)
+            print("\nEND---------------------------------------------------------------------")
+
+main()
 
